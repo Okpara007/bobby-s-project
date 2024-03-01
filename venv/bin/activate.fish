7aee3f8449166679bfe53d274cebc4f2683f3b55
Okpara007 <chinemeremokpara93@gmail.com>
2024-03-01 20:03:38 +0100

    configured

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/delight/denzel_fypII/venv"
+set -gx VIRTUAL_ENV "/Users/delight/Documents/denzel_fypII/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
