9ed6440e0804efdb523a0ffe2bf8e6a451c79d8b
Okpara007 <chinemeremokpara93@gmail.com>
2024-03-03 00:53:42 +0100

    allowed host

--- denzfoods/settings.py
+++ denzfoods/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "false"
 
-ALLOWED_HOSTS = ['abn-realtors.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bobby-s-project.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
